4c4fbfca1270a4ce68c288fc5a7893b0ef0924c3
Samratcodebase <samratcodebase@gmail.com>
2026-02-03 05:41:57 +0530

    Refactor: Assignment-7

---
+++ Assignments_Control_Statements/Assignment-7/WAP_TO_CHECK_EVEN_ODD_WIT_BITWISE.c
@@ -0,0 +1,17 @@
+// WAP to check even odd with BitWise Operator
+#include <stdio.h>
+int main()
+{
+    int num = 12;
+    if (num & 1)
+    {
+        printf("Odd");
+    }
+
+    else
+    {
+        printf("Even");
+
+        return 0;
+    }
+}
