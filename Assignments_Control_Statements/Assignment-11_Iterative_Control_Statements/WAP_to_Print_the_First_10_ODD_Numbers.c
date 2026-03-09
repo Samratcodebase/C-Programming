d3361db468942afd1530d36b41da72da8735a91a
Samratcodebase <samratcodebase@gmail.com>
2026-03-09 17:56:25 +0530

    Write a Program to print the First N odd Numbers

---
+++ Assignments_Control_Statements/Assignment-11_Iterative_Control_Statements/WAP_to_Print_the_First_10_ODD_Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n = 10;
+    while (n)
+    {
+        if (n % 2 == !0)
+        {
+            printf(" %d ", n);
+        }
+        /* code */
+        n--;
+    }
+    return 0;
+}
